da6637820a201d694340eeecbbe8d9a2af1c1ffe
Julius Kamara <juliuskamara79@gmail.com>
2023-02-21 05:10:34 -0800

    Made some changes to the user class

--- 0x00-challenge/3-user.py
+++ 0x00-challenge/3-user.py
@@ -50,9 +50,11 @@ class User():
         - `False` if `__password` is `None`
         - Compare `__password` and the MD5 value of `pwd`
         """
-
-        return bool(self.__password) and hashlib.md5(pwd.encode())\
-.hexdigest().upper() == self.__password
+        if pwd is None or type(pwd) is not str:
+            return False
+        if self.__password is None:
+            return False
+        return hashlib.md5(pwd.encode()).hexdigest().upper() == self.__password
 
 
 if __name__ == '__main__':
